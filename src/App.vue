f5d3629d87cd40e50afaee83ebe4ed0a1784d235
Raymond Lee <15537887071@163.com>
2022-11-27 11:18:17 +0800

    vsbook front static display done

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,38 @@
 <template>
-  <HelloWorld msg="Hello From App" :code=12 @change="change">
-    <!-- 插槽 -->
-    <template #header>
-      Slot from App
-    </template>
-  </HelloWorld>
+<Header></Header>
 
-  <!-- 路由 -->
-  <br/><br/><br/>
-  <router-link to="/">Home</router-link> | 
-  <router-link to="/about">About</router-link>
-  <router-view/>
+<div class="posts">
+  <Post></Post>
+  <Post></Post>
+  <Post></Post>
+  <Post></Post>
+  <Post></Post>
+  <Post></Post>
+  <Post></Post>
+</div>
 
-  <!-- vuex modules -->
-  <br/><br/><br/>
-  <div>
-    当前 vuex count 的求和为：{{countState.sum}}，放大十倍为{{bigSum}}
-    <br/>
-    学校：{{countState.school}}
-    <br/>
-    <button @click="increment">点我+1</button>
-    <button @click="incrementOdd">点我奇数+1</button>
-  </div>
+<Footer></Footer>
 </template>
 
 <script setup lang="ts">
-import HelloWorld from '@/components/HelloWorld.vue'
-import { computed } from 'vue';
-import { useStore } from 'vuex'
-
-function change(id: number) {
-  alert(id)
-}
-
-const store = useStore()
-// state
-const countState = store.state.count
-// getters
-const bigSum = computed(()=>{
-  return store.getters['count/bigSum']
-})
-// mutations
-function increment() {
-  store.commit('count/INCREMENT', 1)
-}
-// actions
-function incrementOdd() {
-  store.dispatch('count/incrementOdd', 1)
-}
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+import Post from './components/Post.vue'
 </script>
 
 <style lang="less" scoped>
+.posts {
+  height: 86vh;
+  overflow-y: scroll;
+}
 
+/* Hide scrollbar for Chrome, Safari and Opera */
+.posts::-webkit-scrollbar {
+    display: none;
+}
+/* Hide scrollbar for IE, Edge and Firefox */
+.posts {
+  -ms-overflow-style: none;  /* IE and Edge */
+  scrollbar-width: none;  /* Firefox */
+}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
+import './assets/css/reset.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
+const app = createApp(App)
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+  }
+
+app.use(store).use(router).mount('#app')
